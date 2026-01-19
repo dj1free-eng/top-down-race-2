9661c8bb37617373f3b5a37486d4ecdb7b840bba
dj1free-eng <dj1free@gmail.com>
2026-01-19 21:17:59 +0000

    Update game.js

--- src/game/game.js
+++ src/game/game.js
@@ -1,16 +1,23 @@
 import Phaser from 'phaser';
 import { BootScene } from './scenes/BootScene.js';
 import { MenuScene } from './scenes/MenuScene.js';
+import { RaceScene } from './scenes/RaceScene.js';
 
 export function createGame(parentId = 'app') {
   return new Phaser.Game({
     type: Phaser.AUTO,
     parent: parentId,
     backgroundColor: '#0b1020',
-    scene: [BootScene, MenuScene],
+    scene: [BootScene, MenuScene, RaceScene],
     scale: {
-      mode: Phaser.Scale.RESIZE,      // usa tamaño real del contenedor
+      mode: Phaser.Scale.RESIZE,
       autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+    physics: {
+      default: 'arcade',
+      arcade: {
+        debug: false
+      }
     },
     render: {
       pixelArt: false,
