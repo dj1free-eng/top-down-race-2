e2ae26bb75f6429563ada5979f8abdf72cecc5a8
dj1free-eng <dj1free@gmail.com>
2026-02-12 02:31:08 +0000

    Update RaceScene.js

--- src/game/scenes/RaceScene.js
+++ src/game/scenes/RaceScene.js
@@ -1768,7 +1768,7 @@ this._devModalContent = this.add.container(0, 0);
 
 // Panel de sliders (compacto)
 const sx = mPanelX + 14;
-let sy = contentTop + 10;
+sy = contentTop + 10;
 const sw = mPanelW - 28;
 
 // Definiciones
