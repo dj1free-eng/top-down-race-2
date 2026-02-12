e2839bd0aeac7c38f156bc0ad81d4c23136a92b6
dj1free-eng <dj1free@gmail.com>
2026-02-12 08:23:30 +0000

    Update game.js

--- src/game/game.js
+++ src/game/game.js
@@ -3,28 +3,30 @@ import { BootScene } from './scenes/BootScene.js';
 import { MenuScene } from './scenes/MenuScene.js';
 import { RaceScene } from './scenes/RaceScene.js';
 
+import { DEV_FACTORY } from './dev/devFlags.js';
+import { CarFactoryScene } from './dev/CarFactoryScene.js';
+
 export function createGame(parentId = 'app') {
+  const scenes = [BootScene, MenuScene, RaceScene];
+  if (DEV_FACTORY) scenes.splice(2, 0, CarFactoryScene); // Boot, Menu, Factory, Race
+
   return new Phaser.Game({
     type: Phaser.AUTO,
     parent: parentId,
     backgroundColor: '#0b1020',
-    scene: [BootScene, MenuScene, RaceScene],
-    dom: {
-  createContainer: true
-},
+    scene: scenes,
+    dom: { createContainer: true },
     scale: {
       mode: Phaser.Scale.RESIZE,
       autoCenter: Phaser.Scale.CENTER_BOTH
     },
     physics: {
       default: 'arcade',
-      arcade: {
-        debug: false
-      }
+      arcade: { debug: false }
     },
     render: {
-  pixelArt: false,
-  antialias: true
-}
+      pixelArt: false,
+      antialias: true
+    }
   });
 }
