459151e1c16bb96b21911f795e6b1797ef592b24
dj1free-eng <dj1free@gmail.com>
2026-02-14 00:51:30 +0000

    Update CarFactoryScene.js

--- src/game/dev/CarFactoryScene.js
+++ src/game/dev/CarFactoryScene.js
@@ -769,10 +769,7 @@ const inspectorContentH = Math.max(0, (iy + 6) - INSPECTOR_TOP);
 this._inspectorScrollMax = Math.max(0, inspectorContentH - INSPECTOR_VIEW_H);
 this._inspectorScroll = Phaser.Math.Clamp(this._inspectorScroll || 0, 0, this._inspectorScrollMax);
 
-const applyInspectorScroll = () => {
-  this._inspectorScroll = Phaser.Math.Clamp(this._inspectorScroll, 0, this._inspectorScrollMax);
-  this._inspectorCont.y = -this._inspectorScroll;
-};
+
 applyInspectorScroll();
 
 // hit-area invisible para capturar drag/scroll dentro del inspector
