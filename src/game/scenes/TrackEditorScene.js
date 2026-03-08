1547c8c16a949ada78ef3163acde1d6bf0b8a970
dj1free-eng <dj1free@gmail.com>
2026-03-08 22:37:03 +0000

    Update TrackEditorScene.js

--- src/game/scenes/TrackEditorScene.js
+++ src/game/scenes/TrackEditorScene.js
@@ -147,9 +147,7 @@ export class TrackEditorScene extends BaseScene {
     this._editCam.setScroll(0, 0);
     this._editCam.setRoundPixels(true);
 // La cámara principal NO debe renderizar el mundo del editor
-this.cameras.main.ignore([
-  canvasPanel
-]); 
+
     // Panel visual del “lienzo”
     const canvasPanel = this.add.graphics().setDepth(5);
     canvasPanel.fillStyle(0xffffff, 0.14);
