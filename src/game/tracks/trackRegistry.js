33b705563ed2113e06130bd5e68270a736c27c90
dj1free-eng <dj1free@gmail.com>
2026-03-13 11:32:27 +0000

    Create trackRegistry.js

---
+++ src/game/tracks/trackRegistry.js
@@ -0,0 +1,17 @@
+import { makeTrack01Oval } from './library/track01/track01_oval.js';
+import { makeTrack02Technical } from './library/track02/track02_technical.js';
+import { makeTrack03Drift } from './library/track03/track03_drift.js';
+
+export const TRACK_REGISTRY = {
+  track01: makeTrack01Oval,
+  track02: makeTrack02Technical,
+  track03: makeTrack03Drift
+};
+
+export function createTrack(trackId) {
+  const factory = TRACK_REGISTRY[trackId];
+  if (!factory) {
+    throw new Error(`Track no encontrado: ${trackId}`);
+  }
+  return factory();
+}
